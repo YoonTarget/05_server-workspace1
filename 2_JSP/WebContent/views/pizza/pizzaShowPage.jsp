a7bf94f79e4f6f2f201e661625de0058223c5a72
YoonTarget <igeek437@gmail.com>
2023-08-07 20:39:12 +0900

    8/7

--- 2_JSP/WebContent/views/pizza/pizzaShowPage.jsp
+++ 2_JSP/WebContent/views/pizza/pizzaShowPage.jsp
@@ -1,5 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%
+
+	String userName = (String)request.getAttribute("userName");
+	String phone = (String)request.getAttribute("phone");
+	String address = (String)request.getAttribute("address");
+	String message = (String)request.getAttribute("message");
+	String pizza = (String)request.getAttribute("pizza");
+	String[] topping = (String[])request.getAttribute("topping");
+	String[] side = (String[])request.getAttribute("side");
+	String payment = (String)request.getAttribute("payment");
+	int price = (int)request.getAttribute("price");
+
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,29 +21,45 @@
 </head>
 <body>
 
-	<%
 	
-		String userName = (String)request.getAttribute("userName");
-		String phone = (String)request.getAttribute("phone");
-		String address = (String)request.getAttribute("address");
-		String message = (String)request.getAttribute("message");
-		String pizza = (String)request.getAttribute("pizza");
-		String[] topping = (String[])request.getAttribute("topping");
-		String[] side = (String[])request.getAttribute("side");
-		String payment = (String)request.getAttribute("payment");
-	
-	%>
-	
-	<h1>피자 주문 정보</h1>
+	<h1>피자 결제 페이지</h1>
+
+	<h2>주문내역</h2>
+
+	<h4>[ 주문자 정보 ]</h4>
 	
 	<ul>
-		<li id="userName">주문자 : <%= userName %></li>
+		<li id="userName">성함 : <%= userName %></li>
 		<li id="phone">전화번호 : <%= phone %></li>
 		<li id="address">주소 : <%= address %></li>
+
+		<% if(message.equals("")) { %>
+		<!-- case1. 요청사항 작성 안 한 경우 -->
+		<li>요청사항 : 작성안함</li>
+		<% } else { %>
+		<!-- case2. 요청사항 작성 한 경우 -->
 		<li id="message">요청사항 : <%= message %></li>
+		<% } %>
+	</ul>
+
+	<br>
+
+	<h4>[ 주문 정보 ]</h4>
+	<ul>
 		<li id="pizza">메뉴 : <%= pizza %></li>
-		<li id="topping">토핑 : <%= String.join("/", topping) %></li>
-		<li id="side">사이드 메뉴 : <%= String.join("/", side) %></li>
+		
+		<% if(topping == null) { %>
+		<li>토핑 : 선택안함</li> <!-- case1 -->
+		<% } else { %>
+		<li id="topping">토핑 : <%= String.join("/", topping) %></li> <!-- case2 -->
+		<% } %>
+
+		<% if(side == null) { %>
+		<li>사이드 메뉴 : 선택안함</li> <!-- case1 -->
+		<% } else { %>
+		<li id="side">사이드 메뉴 : <%= String.join("/", side) %></li> <!-- case2 -->
+		<% } %>
+
 		<li id="payment">결제방식 : 
 		<% if(payment.equals("card")) { %>
 			카드
@@ -39,6 +68,11 @@
 		<% } %>
 		</li>
 	</ul>
+
+	<hr>
+
+	<h3>위와 같이 주문하셨습니다.</h3>
+	<h2>총 가격 : <%= price %>원</h2>
 
 </body>
 </html>
--- 2_JSP/src/main/java/com/kh/controller/PizzaGetServlet.java
+++ 2_JSP/src/main/java/com/kh/controller/PizzaGetServlet.java
@@ -30,6 +30,14 @@ public class PizzaGetServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
+		// 1) 전달값 중에 한글이 있을 경우 인코딩 처리 (post 방식일 때만)
+		// request.setCharacterEncoding("UTF-8");
+		
+		// 2) 요청시 전달값 뽑기 및 데이터 가공처리 (파싱같은 거) => 변수 및 객체 기록
+		// request.getParameter("키값") : "벨류값" (String)
+		// request.getParameterValues("키값") : ["벨류값", "벨류값", ...] (String[])
+		// => 만일 키 값이 존재하지 않을 경우 null 반환
+		
 		String userName = request.getParameter("userName");
 		String phone = request.getParameter("phone");
 		String address = request.getParameter("address");
@@ -38,6 +46,52 @@ public class PizzaGetServlet extends HttpServlet {
 		String[] topping = request.getParameterValues("topping");
 		String[] side = request.getParameterValues("side");
 		String payment = request.getParameter("payment");
+		
+		// 3) 요청처리 (db에 sql문 실행하러 > Service > Dao)
+		
+		int price = 0;
+		
+		switch(pizza) {
+		case "콤비네이션피자" : price += 5000; break;
+		case "치즈피자" : price += 6000; break;
+		case "포테이토피자" : case "고구마피자" : price += 7000; break;
+		case "불고기피자" : price += 8000; break;
+		}
+		
+//		int toppingPrice = 0;
+		
+		if(topping != null) { // 토핑이 null이 아닐 때만 돌리기
+			for(int i = 0; i < topping.length; i++) {
+				
+				/*
+				if(topping[i].equals("고구마무스") || topping[i].equals("콘크림무스")) {
+					toppingPrice += 1500;
+				}
+				else if(topping[i].equals("파인애플토핑") || topping[i].equals("치즈토핑")) {
+					toppingPrice += 200;
+				}
+				else {
+					toppingPrice += 3000;
+				}
+				*/
+				
+				switch(topping[i]) {
+				case "고구마무스" : case "콘크림무스" : price += 1500; break;
+				case "파인애플토핑" : case "치즈토핑" : price += 2000; break;
+				case "치즈바이트" : case "치즈크러스트" : price += 3000; break;
+				}
+			}
+		}
+		
+		if(side != null) { // 사이드가 null이 아닐 때만 돌리기
+			for(int i = 0; i < side.length; i++) {
+				switch(side[i]) {
+				case "콜라" : case "사이다" : price += 2000; break;
+				case "핫소스" : case "갈릭소스" : price += 300; break;
+				case "피클" : case "파마산치즈가루" : price += 500; break;
+				}
+			}
+		}
 		
 		System.out.println("userName : " + userName);
 		
@@ -70,6 +124,14 @@ public class PizzaGetServlet extends HttpServlet {
 		
 		System.out.println("payment : " + payment);
 		
+		System.out.println("price : " + price);
+		
+		// 4) 요청처리 후 사용자가 보게될 응답페이지 (결제페이지) 만들기
+		// 응답페이지(jsp)를 선택해서 포워딩
+		// 단, 응답페이지에서 필요한 데이터가 있다면 담아서 포워딩 할 것!!
+		// request attribute 영역에 담기
+		// 어떤 데이터가 필요한지 모르겠다면? => 먼저 jsp를 만들어보기
+
 		request.setAttribute("userName", userName);
 		request.setAttribute("phone", phone);
 		request.setAttribute("address", address);
@@ -78,8 +140,11 @@ public class PizzaGetServlet extends HttpServlet {
 		request.setAttribute("topping", topping);
 		request.setAttribute("side", side);
 		request.setAttribute("payment", payment);
+		request.setAttribute("price", price);
 		
+		// 응답할 뷰(jsp) 선택
 		RequestDispatcher view =  request.getRequestDispatcher("views/pizza/pizzaShowPage.jsp");
+		// 선택된 뷰가 사용자에게 보여지도록 포워딩
 		view.forward(request, response);
 		
 	}
